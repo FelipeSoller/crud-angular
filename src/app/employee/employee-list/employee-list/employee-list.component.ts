ef0c41209708c403bfad83696ff57234e7c0556e
Felipe Soller <soller@outlook.com>
2021-04-05 13:37:50 -0300

    feat: Configura componente da listagem de funcionários

--- src/app/employee/employee-list/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list/employee-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../../employee.model';
+import { EmployeeService } from '../../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  employees$: Observable<Employee[]>
+
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.employeeList();
+  }
+
+  employeeList() {
+    this.employees$ = this.employeeService.list();
   }
 
 }
