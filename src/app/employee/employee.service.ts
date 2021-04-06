6c86518fc5d4ae9b2f27622982f239b8442543ab
Felipe Soller <soller@outlook.com>
2021-04-06 19:18:23 -0300

    feat: Adiciona métodos de adicionar, deletar e remover funcionários

--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Employee } from './employee.model';
 
 @Injectable({
@@ -26,5 +24,29 @@ export class EmployeeService {
 
   onGet() {
     return this.employees;
+  }
+
+  onGetEmployee(id: Number) {
+    return this.employees.find(employee => employee.id === id);
+  }
+
+  onAdd(employee: Employee) {
+    this.employees.push(employee);
+  }
+
+  onDelete(id: Number) {
+    let employee = this.employees.find(employee => employee.id === id);
+
+    let index = this.employees.indexOf(employee,0);
+
+    this.employees.splice(index,1)
+  }
+
+  onUpdate(employee: Employee) {
+    let oldEmployee = this.employees.find(oldEmployee => oldEmployee.id === employee.id);
+
+    oldEmployee.name = employee.name;
+    oldEmployee.email = employee.email;
+    oldEmployee.phone = employee.phone;
   }
 }
